7e36d11acc16247e2c49ad2302ac94b11ada8448
Robert Smol <robert.smol@stereoit.com>
2012-08-03 13:46:38 +0200

    Basic JSON response on update-kpis

--- src/dashboards/graphs/views.py
+++ src/dashboards/graphs/views.py
@@ -8,26 +8,29 @@ logger = logging.getLogger(__name__)
 
 def update_kpis(request):
     """Update all KPIs to current time"""
-    json_response = {'updated':0, 'kpis': None }}
-    updated_kpi = 0
+    json_response = {'updated':0, 'kpis': [] }
+    updated_kpis = 0
     now = timezone.now()
     for kpi in KPI.objects.all():
         delta = timedelta(seconds=kpi.granularity)
         try:
             latest = kpi.values.latest()
-            difference = (now - latest.timestamp)
-            missing_intervals = (difference.days * 3600 * 24 + difference.seconds) / kpi.granularity
+            interval = (now - latest.timestamp)
+            missing_intervals = (interval.days * 3600 * 24 + interval.seconds) \
+                                        / kpi.granularity
             if missing_intervals > kpi.history:
                 missing_intervals = kpi.history
         except KPIValue.DoesNotExist:
             missing_intervals = kpi.history
         if missing_intervals:
-            update_kpis += 1
-        last_value = now - delta * kpi.history
-        json_response['kpis'].append({
-            'kpi': kpi.name,
-            'last_generated': last_value.strftime('%c'), 
-            'missing_intervals': missing_intervals 
+            updated_kpis += 1
+            last_value = now - delta * kpi.history
+    json_response['kpis'].append({
+        'kpi': kpi.name,
+        'last_generated': last_value.strftime('%c'),
+        'missing_intervals': missing_intervals
         })
-        logger.debug('kpi {0} last generated at {1} missing_intervals {2}'.format(kpi, last_value.strftime('%c'), missing_intervals))
-    return HttpResponse(simplejson.dumps(json_response), mimetype='application/json')
+    logger.debug('kpi {0} last generated at {1} missing_intervals \
+            {2}'.format(kpi, last_value.strftime('%c'), missing_intervals))
+    return HttpResponse(simplejson.dumps(json_response),
+            mimetype='application/json')
