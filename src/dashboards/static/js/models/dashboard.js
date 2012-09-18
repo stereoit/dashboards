14d9f889cd240a9f5770b130a2628cb7d068a327
Robert Smol <robert.smol@stereoit.com>
2012-09-18 16:56:37 +0200

    Updated the dashboard model for proper delayed initialization

--- src/dashboards/static/js/models/dashboard.js
+++ src/dashboards/static/js/models/dashboard.js
@@ -8,40 +8,32 @@ define(['jquery','underscore','backbone','models/kpi','models/colorpalette','col
             this.kpi = new KPI();
             this.kpi.url = this.url()+'kpi';
             this.kpi._loaded = false;
-            this.kpi.on('change', this.checkInitialization, this);
 
             this.palette = new Palette();
             this.palette.url = this.url()+'palette';
             this.palette._loaded = false;
-            this.palette.on('change', this.checkInitialization, this);
 
             this.values = new Values();
             this.values.url = this.url()+'values';
             this.values._loaded = false;
-            this.values.on('change', this.checkInitialization, this);
 
             var self = this;
-            this.kpi.fetch({success: function(model) { self.kpi._loaded = true}});
-            this.palette.fetch({success: function(model) { self.palette._loaded = true}});
-            this.values.fetch({success: function(model) { self.values._loaded = true}});
+            this.kpi.fetch({success: function(model){ self.kpi._loaded = true; self.checkInitialization() } });
+            this.palette.fetch({success: function() { self.palette._loaded = true; self.checkInitialization() } });
+            this.values.fetch({success: function() { self.values._loaded = true; self.checkInitialization() } });
         },
 
         checkInitialization: function(){
-            console.log('Dashboard Model checkInitilization', this);
             if ( ! this.kpi._loaded || ! this.palette._loaded || ! this.values._loaded ) {
-                console.log('checkInitilization did not pass');
                 return;
             } else {
-                console.log('Dashboard Model initilizaiton complete');
 
-                this.kpi.off('change', this.checkInitilization);
-                this.palette.off('change', this.checkInitilization);
-                this.values.off('change', this.checkInitilization);
+                this.kpi.off('change', this.checkInitialization);
+                this.palette.off('change', this.checkInitialization);
+                this.values.off('change', this.checkInitialization);
 
+                console.log('Dashboard Model initialization complete - refreshing every ',this.kpi.get('granularity'),' seconds', this);
 
-                console.log('Dashboard Model refreshing every ',this.kpi.get('granularity'),' seconds', this);
-
-                this.isInitialized = true;   // view can also check for this attribute
                 this.trigger('initialized'); // view listens for this
 
             }
